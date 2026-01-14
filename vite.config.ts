7c54803a8eac094324554285c26c4896f3f86b1b
devendrasaim <mdevendrasai9@gmail.com>
2026-01-14 17:05:24 -0600

    Fixed merge conflict markers in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-<<<<<<< HEAD
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-=======
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
->>>>>>> 341cf735da52260ed6e4908981d32b93d46e8c29
-  server: {
-    host: "::",
-    port: 8080,
-  },
-<<<<<<< HEAD
-  plugins: [
-    react(),
-  ],
-=======
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
->>>>>>> 341cf735da52260ed6e4908981d32b93d46e8c29
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-<<<<<<< HEAD
 });
-=======
-}));
->>>>>>> 341cf735da52260ed6e4908981d32b93d46e8c29
